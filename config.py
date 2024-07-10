058dd4e9db9d9fe1737442c78e0e78b52c55c406
Denis Sergeich <denissergeich@Deniss-MacBook-Air.local>
2024-07-10 13:27:31 +0300

    tracking

---
+++ config.py
@@ -0,0 +1,13 @@
+config = {
+    "logistic_regression": {
+        'penalty': 'l2',
+        'C': 10,
+        'solver': 'saga',
+        'l1_ratio': 0
+    },
+    "gbm_classification": {
+        'max_depth': 4,
+        'learning_rate': 0.1,
+        'n_estimators': 100
+    }
+}
