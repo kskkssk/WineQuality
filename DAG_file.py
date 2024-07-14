c306fa43d0281240f0b07aca3179fb79ee4d78f4
Denis Sergeich <denissergeich@Deniss-MacBook-Air.local>
2024-07-15 00:41:15 +0300

    add dag

--- DAG_file.py
+++ DAG_file.py
@@ -1,7 +1,7 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
-
+from model_train import get_data, train, test, update_model
 
 with DAG(
     'hw_kudasheva',
--- model_train.py
+++ model_train.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import subprocess
 import joblib
-import dvc
 import json
 
 
@@ -16,39 +15,38 @@ def get_data():
 
 
 def train():
-    from config import config
-    X_train, y_train = get_data()
     subprocess.run(['dvc', 'pull'])
     with open('model.pkl', 'rb') as f:
         model = joblib.load(f)
     return model
 
-def metrics_exist(metrics):
-    try:
-        dvc.api.read('metrics.json', remote='s3_mlops')
-        return json.loads(metrics)
-    except:
-        return None
 
 def test():
-    from sklearn.metrics import accuracy_score, f1_score
+    from sklearn.metrics import f1_score
     from datetime import datetime
     model = train()
-    X_test, y_test = get_data()
+    _, X_test, _, y_test = get_data()
     y_pred = model.predict(X_test)
-
-    accuracy = accuracy_score(y_true=y_test, y_pred=y_pred)
     f1 = f1_score(y_true=y_test, y_pred=y_pred, average='macro')
+    return f1
 
-    metrics = {'f1': f1,
-               'accuracy': accuracy,
-               'timestamp': datetime.now()}
 
-    if not metrics_exist(metrics):
-        subprocess.run(['dvc', 'add', 'metrics'])
-    else:
-        current_metrics = metrics
-        current_metrics['f1'] < metrics['f1'] or current_metrics['accuracy'] < metrics['accuracy']:
+def update_model():
+    from datetime import datetime
+    current_f1 = test()
+    subprocess.run(['dvc', 'pull', 'metrics'])
+    try:
+        with open('metrics.json', 'r') as f:
+            current_metrics = json.load(f)
+            if current_metrics['f1'] < current_f1:
+                subprocess.run(['dvc', 'add', 'metrics.json'])
+                subprocess.run(['dvc', 'push'])
+                with open('metrics.json', 'w') as f:
+                    json.dump({'f1': current_f1, 'timestamp': datetime.now()}, f)
+                    print(f"Last score {current_metrics['f1']}")
+                    print(f"Updated model with f1 score: {current_f1}")
+    except:
         subprocess.run(['dvc', 'add', 'metrics'])
         subprocess.run(['dvc', 'push'])
+
 
